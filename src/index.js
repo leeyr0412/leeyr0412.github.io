5fdee0a5518e0e8225713b7932ca00bceaff9113
leeyr0412 <leeyr0412@naver.com>
2023-08-27 00:27:34 +0900

    :lipstick: style: 스타일 설정

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./assets/scss/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
